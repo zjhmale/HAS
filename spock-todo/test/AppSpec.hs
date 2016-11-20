e79bb03d786ef9824c2f8488d1c90eafadaef75a
zjhmale <zjhmale@gmail.com>
2016-11-20 22:42:25 +0800

    use json quasiquote

--- spock-todo/test/AppSpec.hs
+++ spock-todo/test/AppSpec.hs
@@ -5,7 +5,7 @@
 
 module AppSpec where
 
-import Web.Spock
+import Web.Spock hiding (json)
 import Web.Spock.Config
 
 import Model (getAppCfg)
@@ -14,12 +14,12 @@ import Test.Hspec
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Test as Test hiding (request)
 import qualified Test.Hspec.Wai as Test
-import           Data.Aeson (Value(..), object, (.=))
+import           Test.Hspec.Wai.JSON
 
 spec :: Spec
 spec = describe "Todo App" $ do
   Test.with testApp $ it "should response with 'welcome' and 200 status" $
-    Test.get "/" `Test.shouldRespondWith` "{\"msg\":\"welcome\"}" { Test.matchStatus = 200 }
+    Test.get "/" `Test.shouldRespondWith` [json|{msg:"welcome"}|] { Test.matchStatus = 200 }
 
 testApp :: IO Wai.Application
 testApp = do
