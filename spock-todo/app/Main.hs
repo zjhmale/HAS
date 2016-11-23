60965195ad521384e94f26df1f34a2dd4c03d949
zjhmale <zjhmale@gmail.com>
2016-11-23 10:07:05 +0800

    get version

--- spock-todo/app/Main.hs
+++ spock-todo/app/Main.hs
@@ -1,11 +1,14 @@
 module Main where
 
 import           Config
+import           Paths_spock_todo   (version)
 import           Route              (run)
+-- https://www.haskell.org/cabal/users-guide/developing-packages.html#accessing-data-files-from-package-code
 import           System.Environment (lookupEnv)
 
 main :: IO ()
 main = do
+  print version
   env  <- maybe Development read <$> lookupEnv "ENV"
   port <- maybe 3000 read <$> lookupEnv "PORT"
   run port env
